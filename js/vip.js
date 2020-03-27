3d1ed1229389b847af25ad34a0b5a9265fd222ca
leebang <leebang31698@gmail.com>
2020-03-27 15:57:37 -0400

    vip table update

--- js/vip.js
+++ js/vip.js
@@ -1,9 +1,8 @@
 
 
 $(document).ready(function(){
-    // var content = "<h2>jQuery Hello World!</h2>";
 
-    var data_1 = [
+    let data_1 = [
         {
             title:"Personal Assessment I",
             items: [
@@ -15,6 +14,14 @@ $(document).ready(function(){
                         "-", "1.5 hr + Report", "1.5 hr + Report"
                     ]
                 }, 
+                {
+                    title_en: "Academic & Activity Planning",
+                    title_cn: "专业导师进行学术与课外活动的定制规划",
+                    content: "",
+                    packages: [
+                        "-", "-", "1 Hour"
+                    ]
+                }
             ]
         }, 
         {
@@ -25,17 +32,24 @@ $(document).ready(function(){
                     title_cn: "职业规划 & 行业剖析",
                     content: "专业导师进行职业规划与行业剖析",
                     packages: [
-                        "1 Hour", "1 Hour", "1 Hour"
+                        "30 Minutes", "30 Minutes", "1 Hour"
                     ]
                 }, 
+
+            ]
+        }, 
+        {
+            title:"Job Application Strategy",
+            items: [
                 {
-                    title_en: "Academic & Activity Planning",
-                    title_cn: "专业导师进行学术与课外活动的定制规划",
-                    content: "",
+                    title_en: "Personalized Job Application Strategy",
+                    title_cn: "个人定制化工作申请策略",
+                    content: "专业导师进行职业规划与行业剖析",
                     packages: [
-                        "-", "-", "1 Hour"
+                        "-", "1 Hour", "2 Hours"
                     ]
-                }
+                }, 
+
             ]
         }, 
         {
@@ -71,7 +85,7 @@ $(document).ready(function(){
             items: [
                 {
                     title_en: "Networking Skill Development",
-                    title_cn: "建立行业人脉",
+                    title_cn: "人脉拓展技能训练",
                     content: "",
                     packages: [
                         "1 Hour", "1 Hour", "1 Hour"
@@ -79,7 +93,7 @@ $(document).ready(function(){
                 }, 
                 {
                     title_en: "Networking Email Editing",
-                    title_cn: "人脉拓展技能训练",
+                    title_cn: "Networking邮件精修",
                     content: "",
                     packages: [
                         "5 Times", "Unlimited", "Unlimited"
@@ -111,7 +125,7 @@ $(document).ready(function(){
                     title_cn: "沟通技能训练I",
                     content: "",
                     packages: [
-                        "1 Hour", "1 Hour", "3 Hour"
+                        "1 Hour", "1 Hour", "1 Hour"
                     ]
                 }, 
                 {
@@ -119,7 +133,7 @@ $(document).ready(function(){
                     title_cn: "沟通技能训练II",
                     content: "",
                     packages: [
-                        "1 Hour", "1 Hour", "3 Hour"
+                        "1 Hour", "1 Hour", "1 Hour"
                     ]
                 },
                 {
@@ -127,7 +141,7 @@ $(document).ready(function(){
                     title_cn: "沟通技能训练III",
                     content: "",
                     packages: [
-                        "1 Hour", "1 Hour", "3 Hour"
+                        "1 Hour", "1 Hour", "1 Hour"
                     ]
                 },
                 {
@@ -135,7 +149,7 @@ $(document).ready(function(){
                     title_cn: "基础行为面试准备",
                     content: "",
                     packages: [
-                        "2 Hours", "2 Hours", "？？？"
+                        "1 Hour", "1 Hour", "1 Hour"
                     ]
                 },
             ]
@@ -144,8 +158,8 @@ $(document).ready(function(){
             title:"Hard Skill Development",
             items: [
                 {
-                    title_en: "Hard Skill Training Course Access ($3000)",
-                    title_cn: "价值$3000刀硬技能线上培训精品速成小班课",
+                    title_en: "Hard Skill Training Course Access ($2500)",
+                    title_cn: "价值$2500刀硬技能线上培训精品速成小班课",
                     content: "",
                     packages: [
                         "Choose 2 From 5 Courses", "Choose 3 From 5 Courses", "All inclusive"
@@ -157,26 +171,26 @@ $(document).ready(function(){
             title:"Internship Placement",
             items: [
                 {
-                    title_en: "Internship Trianning Program",
+                    title_en: "Internship Trianning Program (Optional)",
                     title_cn: "多伦多本地实习",
                     content: "",
                     packages: [
-                        "8 Weeks", "8 Weeks", "16 Weeks"
+                        "8 Weeks", "8-12 Weeks", "16 Weeks"
                     ]
                 }, 
                 {
-                    title_en: "US Remote Internship Program",
+                    title_en: "US Remote Internship Program (Optional)",
                     title_cn: "美国远程实习",
                     content: "",
                     packages: [
-                        "4 - 6 Weeks", "4 - 6 Weeks", "4 - 6 Weeks"
+                        "4-6 Weeks", "4-6 Weeks", "4-6 Weeks"
                     ]
                 }, 
             ]
         }, 
     ];
 
-    var data_2 = [
+    let data_2 = [
         {
             title:"Personal Assessment II",
             items: [
@@ -185,7 +199,7 @@ $(document).ready(function(){
                     title_cn: "求职竞争力分析",
                     content: "",
                     packages: [
-                        "1 Hour", "1 Hour", "1 Hour"
+                        "1 Hour + Report", "1 Hour + Report", "1 Hour + Report"
                     ]
                 }, 
             ]
@@ -214,7 +228,7 @@ $(document).ready(function(){
                     title_cn: "网申辅导I（针对不同公司的网申准备）",
                     content: "",
                     packages: [
-                        "-", "-", "1 Hour"
+                        "-", "1 Hour", "1 Hour"
                     ]
                 },
                 {
@@ -243,17 +257,25 @@ $(document).ready(function(){
                     title_cn: "行为面试准备",
                     content: "",
                     packages: [
-                        "???", "1 Hour", "1 Hour"
+                        "-", "1 Hour", "1 Hour"
                     ]
                 }, 
                 {
-                    title_en: "Technical/Case Interview Preparation",
+                    title_en: "Technical Interview Preparation",
                     title_cn: "技能面试准备",
                     content: "",
                     packages: [
-                        "???", "2 Hours", "2 Hours"
+                        "-", "2 Hours", "2 Hours"
                     ]
-                }
+                },
+                {
+                    title_en: "Case Interview Preparation",
+                    title_cn: "案例面试准备",
+                    content: "",
+                    packages: [
+                        "-", "2 Hours", "2 Hours"
+                    ]
+                },
             ]
         },
         {
@@ -300,7 +322,7 @@ $(document).ready(function(){
         }
     ];
 
-    var data_3 = [
+    let data_3 = [
         {
             title:"Pre-Employment Prep.",
             items: [
@@ -332,64 +354,90 @@ $(document).ready(function(){
         }, 
     ];
 
-    var allData = [data_1, data_2, data_3];
+    let allData = [data_1, data_2, data_3];
+    let stages = [
+
+        {phase_en: "Phase 1", phase_cn: "求职准备阶段"}, 
+        {phase_en: "Phase 2", phase_cn: "求职冲刺阶段"}, 
+        {phase_en: "Phase 3", phase_cn: "职前辅导阶段"}
+
+    ];
+
     
-    var $tableOne = $("<div></div>");
-    var $tableTwo = $("<div></div>");
-    var $tableThree = $("<div></div>");
+    let $packOne = $("<div></div>");
+    let $packTwo = $("<div></div>");
+    let $packThree = $("<div></div>");
 
-    var tables = [$tableOne, $tableTwo, $tableThree];
+    let packs = [$packOne, $packTwo, $packThree];
 
-for (j = 0; j < tables.length; j++) {
+    
+    for (x = 0; x < stages.length; x++) {
 
-    for (block of allData[j]) {
         
-        // title comp
-        var $title = $(`<h4 class="mb-1"></h4>`);
-        var $title_content = $(`<strong></strong>`);
-        $title_content.append(block.title);
-        $title.append($title_content);
-
-        var $theRow = $(`<div class="row mb-3"></div>`)
-        // item comp
-        for (item of block.items) {
-            //Left column
-            var $left_col = $(`<div class="col-4 mb-2"></div>`);
-            var $title_en = $(`<h5 class="mb-0 text-yellow"></h5>`);
-            var $title_cn = $(`<h6 class="mb-2"></h6>`);
-            var $content = $(`<h8 class="mb-1"></h8>`);
-
-            $title_en.append(item.title_en);
-            $title_cn.append(item.title_cn);
-            $content.append(item.content);
+        for (y = 0; y < allData.length; y++) { 
+            let $phase_en = $(`<h2 class="mb-0"></h2>`);
+            // let $phase_content = $(`<strong></strong>`);
+            $phase_en.append(stages[y].phase_en);
+            // $phase.append($title_content);
+    
+            let $phase_cn = $(`<h5 class="mb-4></h5>`);
+            // let $phase_content = $(`<strong></strong>`);
+            $phase_cn.append(stages[y].phase_cn);
+            // $phase.append($title_content);
+    
+            packs[x].append($phase_en);
+            packs[x].append($phase_cn)
             
-            $left_col.append($title_en);
-            $left_col.append($title_cn);
-            $left_col.append($content);
+            for (block of allData[y]) {
+                let $title_content = $("<strong></strong>");
+                let $title = $(`<h4 class="mb-1"></h4>`);
+                $title_content.append(block.title);
+                $title.append($title_content);
 
-            //Right column
-            var $right_col = $(`<div class="col-md-8 col-md-8 align-self-md-center"></div>`);
-            var $right_col_content = $(`<a class="row text-center"></a>`);
+                packs[x].append($title);
 
-            for (i = 0; i < item.packages.length; i++) {
-                var $thePack = $(`<span class="col-md-4"></span>`);
-                $thePack.append(item.packages[i]);
-                $right_col_content.append($thePack);
+                for (item of block.items) {
+                    let $theRow = $(`<div class="row"></div>`);
+                    let $left_col = $(`<div class="col-4 mb-0"></div>`);
+                    
+                    let $title_en = $(`<h4 class="mb-0 text-yellow"></h4>`);
+                    let $title_cn = $(`<h3 class="mb-2"></h3>`);
+    
+                    $title_en.append(item.title_en);
+                    $title_cn.append(item.title_cn);
+    
+                    $left_col.append($title_en);
+                    $left_col.append($title_cn);
+    
+                    // Right column
+                    let $right_col = $(`<div class="col"></div>`);
+                    let $right_col_content = $(`<a class="job row align-items-center"></a>`);
+    
+                    // Diff pack
+                    let $spanOne = $(`<span class="col-md-6 font-weight-light text-white"></span>`);
+                    let $spanTwo = $(`<span class="col-md-4"></span>`);
+                    let $spanThree = $(`<span class="col-md-3 text-md-right"></span>`);
+    
+                    $spanOne.append(item.packages[x]);
+    
+                    $right_col_content.append($spanOne);
+                    $right_col_content.append($spanTwo);
+                    $right_col_content.append($spanThree);
+                    $right_col.append($right_col_content);
+
+                    $theRow.append($left_col);
+                    $theRow.append($right_col);
+                    packs[x].append($theRow);
+                }
             }
-
-            $right_col.append($right_col_content);
-            $theRow.append($left_col);
-            $theRow.append($right_col);
         }
 
 
-        tables[j].append($title);
-        tables[j].append($theRow);
     }
 
-}
+    $("#packOne").html($packOne);
+    $("#packTwo").html($packTwo);
+    $("#packThree").html($packThree);
 
-    $("#phaseOne").html($tableOne);
-    $("#phaseTwo").html($tableTwo);
-    $("#phaseThree").html($tableThree);
+
 });
